a91393207d8e2bc0df947170d8198f87043418e1
cssssia <cassia.toscani@gmail.com>
2025-06-27 16:39:04 -0300

    asda

--- Assets/Project/Scripts/System/Customers/CustomerAnimation.cs
+++ Assets/Project/Scripts/System/Customers/CustomerAnimation.cs
@@ -6,11 +6,13 @@ public class CustomerAnimation : MonoBehaviour
 
     public void OnEndEntryAnim()
     {
+        Debug.Log("OnEndEntryAnim");
         m_customerBehaviour.Collider.enabled = true;
     }
 
     public void OnStartExitAnim()
     {
+        Debug.Log("OnStartExitAnim");
         m_customerBehaviour.Collider.enabled = false;
     }
 
