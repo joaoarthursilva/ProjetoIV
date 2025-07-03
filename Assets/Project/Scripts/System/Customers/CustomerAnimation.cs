15f40ace0becf661776ea782f258f89db188b5eb
cssssia <cassia.toscani@gmail.com>
2025-07-02 23:10:09 -0300

    tao me testando

--- Assets/Project/Scripts/System/Customers/CustomerAnimation.cs
+++ Assets/Project/Scripts/System/Customers/CustomerAnimation.cs
@@ -3,17 +3,44 @@ using UnityEngine;
 public class CustomerAnimation : MonoBehaviour
 {
     [SerializeField] private CustomerBehaviour m_customerBehaviour;
+    [SerializeField] private GameObject m_doroteaIdle;
+    private Animator m_animator;
 
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    Vector3 odeioOpedro;
     public void OnEndEntryAnim()
     {
         Debug.Log("OnEndEntryAnim");
         m_customerBehaviour.Collider.enabled = true;
+
+        if (m_doroteaIdle == null)
+        {
+            odeioOpedro = transform.localPosition;
+            transform.localPosition = new Vector3(0.68f, odeioOpedro.y, odeioOpedro.z);
+            m_animator.Play("Idle");
+        }
+        else
+        {
+            m_doroteaIdle.SetActive(true);
+            odeioOpedro = transform.localPosition;
+            transform.localPosition = new Vector3(odeioOpedro.x, odeioOpedro.y + 50f, odeioOpedro.z);
+        }
     }
 
     public void OnStartExitAnim()
     {
         Debug.Log("OnStartExitAnim");
+        transform.localPosition = odeioOpedro;
         m_customerBehaviour.Collider.enabled = false;
+
+        if (m_doroteaIdle != null)
+        {
+            m_doroteaIdle.SetActive(false);
+        }
     }
 
 
